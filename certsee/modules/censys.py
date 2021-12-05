c631d8e774848f13fb0f94233288b7075b98f06d
h0ffayyy <6510183+h0ffayyy@users.noreply.github.com>
2021-12-05 21:12:04 +0000

    update censys search to start query with domain name

--- certsee/modules/censys.py
+++ certsee/modules/censys.py
@@ -1,23 +1,49 @@
 import requests
 
-
-# build censys object
 class Censys:
-    def __init__(self):
-        pass
+    """Contains information about results from a Censys.io search
 
-    # query API by certificate serial
-    def query_serial(self):
+    Attributes:
+        fingerprints - a list of sha256 fingerprints for matching certificates
+    
+    """
+    def __init__(self, domain, timeframe):
+        self.domain_name = domain
+        self.fingerprints = self.query_serial(self.domain_name)
+        self.lookback = timeframe
 
-        certificate_serial="13955790621440138352670111411558754022"
+    # TODO: only search for certificates within lookback
+    # parsed.validity.start: [2021-12-03 to 2021-12-05]
+    # need to get current date, subtract lookback time
 
-        request_body = {"query":f"parsed.serial_number: {certificate_serial}", 
+    def query_serial(self, domain):
+        """Query Censys.io for certificates that match a given domain name
+
+        Params:
+            self - N/A
+
+        """
+
+        domain_name=""
+        certificate_fingerprints = []
+
+        request_body = {"query":f"parsed.names: {domain_name}", 
                         "page": 1, 
                         "flatten": False}
         headers = {"Content-Type":"application/json"}
 
-        res = requests.post(url="https://search.censys.io/api/v1/search/certificates", 
-                            json=request_body,
-                            auth=(api_id, api_secret))
+        try:
+            res = requests.post(url="https://search.censys.io/api/v1/search/certificates", 
+                                json=request_body,
+                                auth=(api_id, api_secret))
 
-        print(res.content)
+            # add fingerprints to list
+            results = res.json()['results']
+            print(results)
+            for result in results:
+                certificate_fingerprints.append(result['parsed']['fingerprint_sha256'])
+
+            return certificate_fingerprints
+        except Exception as e:
+            print(e)
+            
